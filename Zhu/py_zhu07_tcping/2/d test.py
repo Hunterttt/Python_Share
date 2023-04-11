3af546f7b485c8ef6d21842c6de412eb801304be
Hunterttt <tianyuan.ttt@gmail.com>
2023-04-11 12:54:39 +0900

    20230411

--- Zhu/py_zhu07_tcping/2/d test.py
+++
@@ -1,6 +0,0 @@
-d = {'name':'tony','sex':'male'}
-
-a = 'name'
-
-print(d['name'])
-print(d[a])
--- Zhu/py_zhu19_Loop CPU/cre_exl.py
+++
@@ -1,8 +0,0 @@
-import os,sys
-import openpyxl
-
-os.chdir(sys.path[0]) 
-
-wb = openpyxl.Workbook()
-ws = wb.active
-wb.save('show_cpu.xlsx')
